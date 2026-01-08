6736a7b971fee579e2e65820cf2d24a5a97cfa7e
Kostya <eadors@gmail.com>
2026-01-08 22:20:33 +0200

    commit

--- argocd/variables.tf
+++ argocd/variables.tf
@@ -8,3 +8,14 @@ variable "cluster_name" {
   default = "cluster1"
 }
 
+variable "app_repo_url" {
+ description = "Public Git-repo with manifests"
+ type    = string
+ default   = "https://github.com/KostiantynMoskalenko/goit-argo.git"
+}
+
+variable "app_repo_branch" {
+ description = "Branch"
+ type    = string
+ default   = "main"
+}
