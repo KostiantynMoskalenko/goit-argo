3651ae836f9514f3362bbb443cf306a410a64b96
Kostya <eadors@gmail.com>
2026-01-08 23:36:53 +0200

    commit

--- argocd/main.tf
+++ argocd/main.tf
@@ -24,18 +24,63 @@ data "aws_eks_cluster_auth" "eks" {
   name = var.cluster_name
 }
 
-resource "kubernetes_namespace" "infra_tools" {
+resource "kubernetes_namespace" "argo" {
   metadata {
-    name = "infra-tools"
+    name = var.argocd_namespace
   }
 }
 
-resource "helm_release" "argocd" {
+resource "helm_release" "argo" {
   name       = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  namespace  = kubernetes_namespace.infra_tools.metadata[0].name
+  namespace  = kubernetes_namespace.argo.metadata[0].name
+  version    = var.argocd_chart_version
   create_namespace = true
   values = [file("${path.module}/values/argocd-values.yaml")]
 }
 
+resource "kubernetes_manifest" "namespaces_appset" {
+ manifest = {
+  apiVersion = "argoproj.io/v1alpha1"
+  kind    = "ApplicationSet"
+  metadata = {
+   name   = "namespaces-appset"
+   namespace = var.argocd_namespace
+  }
+  spec = {
+   generators = [{
+    git = {
+     repoURL   = var.app_repo_url
+     revision  = var.app_repo_branch
+     directories = [{ path = "namespace/*" }]
+    }
+   }]
+   template = {
+    metadata = {
+     name   = "ns-{{path.basename}}"
+     namespace = var.argocd_namespace
+    }
+    spec = {
+     project = "default"
+     source = {
+      repoURL    = var.app_repo_url
+      targetRevision = var.app_repo_branch
+      path      = "{{path}}"
+      directory = { recurse = true } # читати підкаталоги (apps/, configmaps/, secrets/)
+     }
+     destination = {
+      server  = "https://kubernetes.default.svc"
+      namespace = "{{path.basename}}" # application / infra-tools / ...
+     }
+     syncPolicy = {
+      automated  = { prune = true, selfHeal = true }
+      syncOptions = ["CreateNamespace=true"]
+     }
+     revisionHistoryLimit = 2
+    }
+   }
+  }
+ }
+ depends_on = [helm_release.argo]
+}
--- argocd/variables.tf
+++ argocd/variables.tf
@@ -8,6 +8,18 @@ variable "cluster_name" {
   default = "cluster1"
 }
 
+variable "argocd_namespace" {
+ description = "Namespace for Argo CD"
+ type    = string
+ default   = "infra-tools"
+}
+
+variable "argocd_chart_version" {
+ description = "Helm-chart version Argo CD"
+ type    = string
+ default   = "v7.7.5"
+}
+
 variable "app_repo_url" {
  description = "Public Git-repo with manifests"
  type    = string
